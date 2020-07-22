29700443811245b382b54dda9fde6ad0341b2991
Muhammad <bashirmusa748@gmail.com>
2020-07-22 18:19:40 +0100

    Add files via upload

---
+++ src/quizService/index.js
@@ -0,0 +1,375 @@
+const qBank = [
+  {
+    question:
+      "How many chapters are there in the Holy Quran?   ",
+    answers: ["112", "114", "666", "662"],
+    correct: "114",
+    questionId: "099099"
+  },
+  {
+    question:
+      'Where was the Holy Prophet Muhammad SAW born?',
+    answers: ["Mecca", "Madina", "Czech Republic", "Dubai"],
+    correct: "Mecca",
+    questionId: "183452"
+  },
+  {
+    question:
+      "What is the name of the first battle fought in Islam?",
+    answers: ["Badr", "Uhud", "World-war 2", "khandar"],
+    correct: "HTC",
+    questionId: "Badr"
+  },
+  {
+    question: "What's the name of the Holy prophet's parents?",
+    answers: [
+      "Abdullahi & Amina",
+      "Abdullah & Ummulkhair",
+      "Musa & Amina",
+      "Abbas & Suwaiba"
+    ],
+    correct: "Abdullahi & Amina",
+    questionId: "333247"
+  },
+  {
+    question: "What is the most common name in the world?",
+    answers: ["Umma", "Muhammad", "John", "Evans"],
+    correct: "Muhammad",
+    questionId: "496293"
+  },
+  {
+    question:
+      "which scripture was revealed to prophet Musa (AS)?",
+    answers: [
+      "Zabur",
+      "Taurah",
+      "Oxford dictionary",
+      "Ingeel"
+    ],
+    correct: "Ingeel",
+    questionId: "588909"
+  },
+  {
+    question:
+      'Who was the first caliph after the death of the prophet Muhammad (PBUH)?',
+    answers: ["Abu bakr", "Peter", "Mufti Menk", "Umar Bn Khattab"],
+    correct: "Abu bakr",
+    questionId: "648452"
+  },
+  {
+    question: "What is the first book of the Old Testament?",
+    answers: ["Genesis", "Exodus", "Leviticus", "Numbers"],
+    correct: "Genesis",
+    questionId: "786649"
+  },
+  {
+    question:
+      "Which of the companions did the prophet Muhammad (PBUH) enjoin the responsibility of the adhan?",
+    answers: ["Bilal ibn Rabah", "None of the above", "Abdur-Rahman Sudais", "Abdullah Bn Masood"],
+    correct: "Bilal ibn Rabah",
+    questionId: "839754"
+  },
+  {
+    question:
+      "who is the fourth of the rightly guided Caliph after the life of Prophet Muhammad(PBUH)?",
+    answers: [
+      "Ibn Taimiyyah",
+      "Uthman Bn Affan",
+      "Sheikh Jafar",
+      "Ali Bn Abu Talib"
+    ],
+    correct: "Ali Bn Abu Talib",
+    questionId: "98390"
+  },
+  {
+    question: "Who is the first matyr in islam?",
+    answers: ["Khadija", "Sumayya", "Balls", "Fatima"],
+    correct: "Sumayya",
+    questionId: "1071006"
+  },
+  {
+    question: "What is the third month of of the islamic calender?",
+    answers: ["Rabiul sani", "Rabiul Awwal", "Jumada Awwal", "March"],
+    correct: "Rabiul Awwal",
+    questionId: "1174154"
+  },
+  {
+    question: "Area 51 is located in which US state?",
+    answers: ["Nevada", "Arizona", "New Mexico", "Utah"],
+    correct: "Nevada",
+    questionId: "1226535"
+  },
+  {
+    question: "How would one say goodbye in Spanish?",
+    answers: ["Adiós", " Hola", "Au Revoir", "Salir"],
+    correct: "Adiós",
+    questionId: "1310938"
+  },
+  {
+    question: "What is the name of tribe of our Holy Prophet?",
+    answers: ["Quraysh", "Arabian", "Hashim", "Kanuri"],
+    correct: "Quraysh",
+    questionId: "1436873"
+  },
+  {
+    question: "What is the name of the Prophet nurse?",
+    answers: ["Halimatus Saadiya", "Ummulkhair", "Ummu Salma", "Nurse Zahra"],
+    correct: "Halimatus Saadiya",
+    questionId: "1515110"
+  },
+  {
+    question: "What is the name of the second battle in islam?",
+    answers: ["Badr", "Cold War", "civil War", "Uhud"],
+    correct: "Uhud",
+    questionId: "1642728"
+  },
+  {
+    question:
+      "Who is the first man to accept islam? ",
+    answers: ["Muhammadu Buhari", "Ali Bn Abu Talib", "Abu Bakr", "Usman Danfodio"],
+    correct: "Abu Bakr",
+    questionId: "1747256"
+  },
+  {
+    question:
+      "Who si the first woman to accept islam?",
+    answers: ["Maryam Lemu", "Khadija", "Aisha", "Queen Amina"],
+    correct: "Khadija",
+    questionId: "1822532"
+  },
+  {
+    question: "Who is the first slave to accept islam?",
+    answers: [
+      "Malcom X",
+      "Bilal Bn Rabah",
+      "Abraham Lincoln",
+      "Zayd"
+    ],
+    correct: "Zayd",
+    questionId: "195075"
+  },
+  {
+    question: 'which African country did some group of Sahabas migrated to during Prophet time?',
+    answers: ["Mauritania", "Ethopia", "Egypt", "Nigeria"],
+    correct: "Ethopia",
+    questionId: "2019778"
+  },
+  {
+    question: "What is the name of the Jewish New Year?",
+    answers: ["Rosh Hashanah", "Elul", "New Year", "Succoss"],
+    correct: "Rosh Hashanah",
+    questionId: "2134343"
+  },
+  {
+    question: "Who was the king of Habash when Muslims migrated there?",
+    answers: [
+      "Ado Bayero",
+      "Najashi",
+      "Aalafin Oyo",
+      "Fahad"
+    ],
+    correct: "Najashi",
+    questionId: "2210799"
+  },
+  {
+    question:
+      "What is the name of the Sahab that represented the group of muslims that migrated to Africa?",
+    answers: ["Nasir Albani", "Jafar Bn Abu Talib", "Umar Abdul Azeez", "Ali Bn Abu Talib"],
+    correct: "Jafar Bn Abu Talib",
+    questionId: "2379831"
+  },
+  {
+    question: "What is the name of the Animal that the Prophet rode on his journey to heaven?",
+    answers: ["Shrek", "Buraq", "Wave Rider", "Dragon"],
+    correct: "Buraq",
+    questionId: "2426418"
+  },
+  {
+    question: "Which Surah is called the heart of the Quran?",
+    answers: ["Ikhlas", "Baqarah", "Yasin", "Al Fatiha"],
+    correct: "Yasin",
+    questionId: "2510086"
+  },
+  {
+    question: "What is the name of the cave where revelation of Holy Qyran started?",
+    answers: [
+      "Cave of Hira",
+      "Cave of Mayan",
+      "Zuma rock",
+      "Cave of Thawr"
+    ],
+    correct: "Cave of Hira",
+    questionId: "2685745"
+  },
+  {
+    question:
+      "What is the old name of of Madinatul Munawwarah?",
+    answers: ["Adis Ababa", "Yathrib", "Zaria", "Cairo"],
+    correct: "Yathrib",
+    questionId: "2796884"
+  },
+  {
+    question: "What is the name of the village the Prophet mother was buried?",
+    answers: ["Nairobi", "Tokyo", "Riyaad", "Abwaab"],
+    correct: "Abwaab",
+    questionId: "2838900"
+  },
+  {
+    question: 'Which of the following is not a source of Shariah(Islamic Law)?',
+    answers: ["Sunnah", "Quran", " Qiyas", "Fatwa"],
+    correct: "Fatwa",
+    questionId: "298060"
+  },
+  {
+    question: "which of the following is the largest Muslim country in the world by population?",
+    answers: ["Syria", "Saudi Arabia", "Iran", "Indonesia"],
+    correct: "Indonesia",
+    questionId: "3096579"
+  },
+  {
+    question: "how many prophets were mentioned in the holy Quran?",
+    answers: [
+      "20",
+      "25",
+      "30",
+      "35"
+    ],
+    correct: "25",
+    questionId: "3182461"
+  },
+  {
+    question: "What is the name of the longest chapter in holy Quran?",
+    answers: [
+      "Anfaal",
+      "Genesis",
+      "Baqarah",
+      "Al Imraan"
+    ],
+    correct: "Baqarah",
+    questionId: "3239112"
+  },
+  {
+    question: "how many verses are there in the holy Quran?",
+    answers: ["6326", "6236", "6790", "6666"],
+    correct: "6236",
+    questionId: "3318503"
+  },
+  {
+    question:
+      'What is the name of the shortest chapter in the holy Quran',
+    answers: ["Al Fatiha", "Nas", "Ikhlas", "Al Kawthar"],
+    correct: "Al Kawthar",
+    questionId: "3410327"
+  },
+  {
+    question: "How many words are there in the Holy Quran?",
+    answers: ["98000", "67000", "89000", "79000"],
+    correct: "79000",
+    questionId: "3542596"
+  },
+  {
+    question: "how many letters are there in the Holy Quran",
+    answers: ["312839", "343890", "323670", "329000"],
+    correct: "323670",
+    questionId: "3667517"
+  },
+  {
+    question: "Who introduced the islamic calender?",
+    answers: [
+      "Umar Bn Khattab",
+      "Ibn Qayyum",
+      "Abubakar Gumi",
+      "Uthman Bn Affaan"
+    ],
+    correct: "Umar Bn Khattab",
+    questionId: "3791672"
+  },
+  {
+    question: "Who built the first islamic navy?",
+    answers: ["Usman DanFodio", "Shehu Inyas", "Abu Bakr", "Uthman Bn Affaan"],
+    correct: "Uthman Bn Affaan",
+    questionId: "3893585"
+  },
+  {
+    question: 'Give one Arabic word for the unity of Allah?',
+    answers: ["Seerah", "Tajweed", "Ikhlas", "Tawheed"],
+    correct: "Tawheed",
+    questionId: "3913430"
+  },
+  {
+    question: "How tall is the Burj Khalifa?",
+    answers: ["2,722 ft", "2,717 ft", "2,546 ft", "3,024 ft"],
+    correct: "2,722 ft",
+    questionId: "4049121"
+  },
+  {
+    question:
+      "Whic is the term for breaking of the fast?",
+    answers: ["Uno", "Kareem", "Sahoor", "Iftar"],
+    correct: "Iftar",
+    questionId: "4150746"
+  },
+  {
+    question:
+      "What is the number of Makki surahs in Quran?",
+    answers: ["90", "91", "98", "93"],
+    correct: "93",
+    questionId: "4235063"
+  },
+  {
+    question: "What is the number of madani surahs in Quran?",
+    answers: ["22", "27", "21", "24"],
+    correct: "21",
+    questionId: "4321002"
+  },
+  {
+    question: "What will be the spoken language in paradise?",
+    answers: [
+      "Hausa",
+      "Arabic",
+      "Spanish",
+      "Hebrew"
+    ],
+    correct: "Arabic",
+    questionId: "4442286"
+  },
+  {
+    question: "How many letters are there in Arabic Language?",
+    answers: [
+      "29",
+      "28",
+      "27",
+      "30"
+    ],
+    correct: "29",
+    questionId: "4564430"
+  },
+  {
+    question: "Value of salat in MAsjidil Aqsa?",
+    answers: ["200", "1000", "500", "600"],
+    correct: "500",
+    questionId: "4630606"
+  },
+  {
+    question:
+      "How many letters have dots around them?",
+    answers: ["20", "35", "25", "15"],
+    correct: "15",
+    questionId: "4795960"
+  },
+  {
+    question: "How many letters do not have dots around them?",
+    answers: ["24", "13", "33", "23"],
+    correct: "13",
+    questionId: "4811162"
+  },
+  {
+    question: "What is the first month of the Islamic calender?",
+    answers: ["Safar", "January", "Muharram", "Shawwal"],
+    correct: "Muharram",
+    questionId: "4982981"
+  }
+];
+
+export default (n = 5) =>
+  Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
